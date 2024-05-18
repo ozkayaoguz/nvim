9f0fc65079c2c9ff8a907da06cd23b80b3cfd7d6
Oguzhan Ozkaya <oguzdt@gmail.com>
2024-05-18 18:52:13 +0300

    harpoon

---
+++ lua/ozkayaoguz/plugins/harpoon.lua
@@ -0,0 +1,43 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local harpoon = require("harpoon")
+		local extensions = require("harpoon.extensions")
+
+		harpoon:setup()
+		harpoon:extend(extensions.builtins.navigate_with_number())
+
+		vim.keymap.set("n", "<C-a>", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<leader>h", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+		vim.keymap.set("n", "<C-p>", function()
+			harpoon:list():prev()
+		end)
+		vim.keymap.set("n", "<C-n>", function()
+			harpoon:list():next()
+		end)
+
+		harpoon:extend({
+			UI_CREATE = function(cx)
+				vim.keymap.set("n", "<C-v>", function()
+					harpoon.ui:select_menu_item({ vsplit = true })
+				end, { buffer = cx.bufnr })
+
+				vim.keymap.set("n", "<C-x>", function()
+					harpoon.ui:select_menu_item({ split = true })
+				end, { buffer = cx.bufnr })
+
+				vim.keymap.set("n", "<C-t>", function()
+					harpoon.ui:select_menu_item({ tabedit = true })
+				end, { buffer = cx.bufnr })
+			end,
+		})
+	end,
+}
